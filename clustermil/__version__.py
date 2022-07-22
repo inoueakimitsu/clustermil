a17da292eb8fafe4a75764bd39f6fa767bd3df04
Akimitsu Inoue <akimitsu.inoue@gmail.com>
2022-07-22 14:08:34 +0000

    increment version

--- clustermil/__version__.py
+++ clustermil/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the clustermil package.
 
 __title__ = 'clustermil'
 __description__ = 'clustermil - clustering based multiple instance learning'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Akimitsu Inoue'
 __author_email__ = 'akimitsu.inoue@gmail.com'
 __license__ = 'MIT'
